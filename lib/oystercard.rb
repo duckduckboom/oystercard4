a60c711e8f17a835ff89f7f6ae087da8c6ff0507
Richard Wardingley <richardwardingley@hotmail.com>
2021-09-28 15:45:51 +0100

    chapter 7: deducting money

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -7,7 +7,10 @@ class Oystercard
     end
 
     def top_up(top_up_amount)
-    
         (@balance + top_up_amount > BALANCE_LIMIT) ? "Card limit of #{BALANCE_LIMIT} will be exceeded." : @balance += top_up_amount
+    end    
+
+    def deduct(fare)
+        @balance -= fare
     end    
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,27 +1,39 @@
 require "oystercard"
 
 describe Oystercard do
-    it "intialise card balance" do
-        expect(subject.initialise(0)).to eq(0)
+
+  balance_limit = Oystercard::BALANCE_LIMIT
+
+  it "intialise card balance" do
+      expect(subject.initialise(0)).to eq(0)
+  end
+
+  describe '#top_up' do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it "adds an amount to the balance" do
+      subject.initialise(0)
+      expect(subject.top_up(10)).to eq(subject.balance)
+    end  
+
+    it "refuses to top-up above balance limit" do
+      subject.initialise(0)
+      expect(subject.top_up(balance_limit + 1)).to eq("Card limit of #{balance_limit} will be exceeded.") 
+    end
+  end
+
+  describe 'deduct balance' do
+
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+    
+    it "deducts fare from card balance" do
+      subject.initialise(balance_limit)
+      expect(subject.deduct(30)).to eq(subject.balance)
     end
 
-    describe '#top_up' do
 
-      it { is_expected.to respond_to(:top_up).with(1).argument }
 
-      it "adds an amount to the balance" do
-        subject.initialise(0)
-        expect(subject.top_up(10)).to eq(subject.balance)
-      end  
+  end
 
-      it "refuses to top-up above balance limit" do
-       balance_limit = Oystercard::BALANCE_LIMIT
-        subject.initialise(0)
-        expect(subject.top_up(balance_limit + 1)).to eq("Card limit of #{balance_limit} will be exceeded.") 
-      end
-    #   If a user is going to top-up their card up above the limit, we expect this to refuse their top-up and raise a warning.
-
-    end 
 end    
-
- 
